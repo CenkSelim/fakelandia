c15331564d9dc646933bcd2ce734ad5cab21f0d5
Cenk <muzzyathome@gmail.com>
2022-04-24 11:41:17 +0100

    Added image for under construction

--- src/components/confession/confession.tsx
+++ src/components/confession/confession.tsx
@@ -1,6 +1,8 @@
+import image from '../../assets/under_construction.png'
 const Confession: React.FC = () => 
   <div>
-    <h2>Confession</h2>
+    <h2>Confession</h2> 
+    <img src={image} alt='Under construction!'/>
   </div>;
 
 export default Confession;
--- src/components/misdemeanour/misdemeanour.tsx
+++ src/components/misdemeanour/misdemeanour.tsx
@@ -1,6 +1,9 @@
+import image from '../../assets/under_construction.png'
+
 const Misdemeanour : React.FC = () =>
   <div>
     <h2>Misdemeanour</h2>
+    <img src={image} alt='Under construction!'/>
   </div>;
 
 export default Misdemeanour;
